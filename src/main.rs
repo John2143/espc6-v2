46e49956246b3982491fa18459f01383afef92f2
John Schmidt <john_schmidt@discovery.com>
2024-08-08 12:02:08 +0200

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use esp_idf_svc::hal::delay::FreeRtos;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -6,5 +8,9 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    for i in 0..10 {
+        log::info!("Hello, world! {}", i);
+        // sleep 1 second
+        FreeRtos::delay_ms(1000);
+    }
 }
